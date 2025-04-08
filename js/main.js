9c79369be6fc62023270514a614f6deb009b24a4
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-08 15:22:25 +0200

    dichiaro la funzione

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+Il saluto deve essere :
+buongiorno se è mattina (fino alle 13), 
+buon pomeriggio se è pomeriggio (fino alle 17) 
+e buonasera se è sera (oltre le 17)
+*/
+
+const name = 'Mario';
+const userGreeting = prompt("Fai un saluto:")
+console.log(userGreeting);
+
+// Dichiara la funzione qui.
+function greeting(morning, afternoon, dinner) {
+    for (let i = 0; i < greeting.length; i++) {
+    }
+    
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+
+//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
